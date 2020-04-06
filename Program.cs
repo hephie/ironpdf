c79e852a4f1232946ccb776551014e6079314b25
Thomas Gielissen <thomas_gielissen@pandora.be>
2020-04-06 11:18:12 +0200

    sample for ironpdf
    want to control the landscape flag via css if possible. Not working at the moment.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace HtmlToPdfNeedLandscapeFromCss
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pathToview = Path.Combine(Environment.CurrentDirectory, "FinalReport.cshtml");
+            var htmlWithoutModelForNow = System.IO.File.ReadAllText(pathToview);
+
+            var Renderer = new IronPdf.HtmlToPdf();
+            var outputPath = Path.Combine(Environment.CurrentDirectory, "output.pdf");
+            var pdfDoc = Renderer.RenderHtmlAsPdf(htmlWithoutModelForNow).SaveAs(outputPath);
+
+        }
+    }
+}
